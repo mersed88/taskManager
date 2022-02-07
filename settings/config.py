36a5e831a0cfa7037946a73c3fe52ea86509bebb
Konstantin <mersed@ya.ru>
2022-02-07 22:53:19 +0300

    first

--- settings/config.py
+++ settings/config.py
@@ -25,7 +25,7 @@ PYTHON_PROFILES_ACTIVE = os.getenv('PYTHON_PROFILES_ACTIVE', 'dev')
 BRANCH = os.getenv('BRANCH', 'master')
 URL_PROFILE = CONFIG_URL + '/' + NAME_PROJECT + '-' + PYTHON_PROFILES_ACTIVE + '.yml'
 elastic_host = ELASTIC_HOST
-elastic_host = elastic_host if not 'http://' in elastic_host else elastic_host.replace('http://', '')
+# elastic_host = elastic_host if not 'http://' in elastic_host else elastic_host.replace('http://', '')
 # elastic_host, elastic_port = elastic_host.split(':')
 
 handler = None
@@ -40,15 +40,15 @@ handler = None
 #     handler = None
 
 logger.info(f'''urlProfile = {URL_PROFILE}''')
-task = yaml.load(requests.get(URL_PROFILE).text, Loader=yaml.Loader)
+# task = yaml.load(requests.get(URL_PROFILE).text, Loader=yaml.Loader)
 
 try:
 
     user_postgres = os.getenv("USER_POSTGRES", "postgres")
-    password_postgres = os.getenv("PASSWORD_POSTGRES", "4265d93e7d8aa3893fc899f4414fbef9d2eddc5913ec76201ae11bb8fc1eea89")
-    host_postgres = os.getenv("HOST_POSTGRES", "ec2-52-214-125-106.eu-west-1.compute.amazonaws.com")
+    password_postgres = os.getenv("PASSWORD_POSTGRES", "Nhjkm123")
+    host_postgres = os.getenv("HOST_POSTGRES", "188.120.237.127")
     port_postgres = os.getenv("PORT_POSTGRES", "5432")
-    db_postgres = os.getenv("DB_POSTGRES", "dei9is9m72ghmj")
+    db_postgres = os.getenv("DB_POSTGRES", "postgres")
 
     engine_string = f'postgresql+asyncpg://{user_postgres}:{password_postgres}@{host_postgres}:{port_postgres}/{db_postgres}'
     schema_db = os.getenv("SCHEMA", "taskManager")
--- src/get_models.py
+++ src/get_models.py
@@ -6,12 +6,12 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import Session
 
-from models.tables import ClientsPhone
+from models.tables import ScheduleDaily
 from settings.config import logger
 from datetime import datetime
 
 
-async def get_task_by_worker(session: Session, id: str) -> WorkerTask:
+async def get_task_by_worker(session: Session, id: str) -> ScheduleDaily:
     """
     Получение задания
     :param session: сессия с БД
@@ -21,13 +21,13 @@ async def get_task_by_worker(session: Session, id: str) -> WorkerTask:
     try:
         current_datetime = datetime.now()
         logger.info(f"Get task")
-        stmt = select(ScheduleDaily.pickle_id).
-        where(ScheduleDaily.sch_month == current_datetime.month).
-        where(ScheduleDaily.sch_day == current_datetime.day).
+        stmt = select(ScheduleDaily.pickle_id).\
+        where(ScheduleDaily.sch_month == current_datetime.month).\
+        where(ScheduleDaily.sch_day == current_datetime.day).\
         where(ScheduleDaily.sch_hour == current_datetime.hour)
         result = await session.execute(stmt)
         return result.first()
 
     except Exception as e:
         logger.error(f"Failed get task \n {e}")
-        return WorkerTask()
+        return ScheduleDaily()
