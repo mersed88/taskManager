05cd8f73142dc41421e7dae977b80a65529f5673
alex <sasha10041998@mail.ru>
2022-02-08 19:57:31 +0300

    fix relationship

--- models/tables.py
+++ models/tables.py
@@ -1,20 +1,17 @@
 # coding: utf-8
 
-import datetime
+from sqlalchemy import Column, Integer, String, event, LargeBinary
+from sqlalchemy import ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import Session
-from sqlalchemy import Column, Integer, String, Text, MetaData, DDL, event, Float, JSON, Boolean, Sequence, Identity, \
-    Date, LargeBinary
-from sqlalchemy import create_engine, ForeignKey
 from sqlalchemy.orm import relationship
-from sqlalchemy.dialects.postgresql import UUID, BYTEA
-from sqlalchemy.types import DateTime
 
-from settings.config import schema_db, engine_string
+from settings.config import schema_db
 
 SCHEMA = schema_db
 Base = declarative_base()
-event.listen(Base.metadata, 'before_create', DDL(f"Create schema if not exists {SCHEMA}"))
+from sqlalchemy.schema import CreateSchema
+
+event.listen(Base.metadata, 'before_create', CreateSchema(SCHEMA))
 
 class Scenario(Base):
     __tablename__ = 'scenario'
@@ -23,7 +20,7 @@ class Scenario(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     name_scenario = Column(String)
     pickle = Column(LargeBinary)
-    children = relationship("ScheduleDaily", back_populates="parent")
+    children = relationship("ScheduleDaily")
 
 
 class ScheduleDaily(Base):
@@ -34,7 +31,7 @@ class ScheduleDaily(Base):
     sch_month = Column(Integer)
     sch_day = Column(Integer)
     sch_hour = Column(Integer)
-    pickle_id = Column(Integer, ForeignKey('scenario.id'))
+    pickle_id = Column(Integer, ForeignKey(f'{SCHEMA}.scenario.id'))
     parent = relationship("Scenario", back_populates="children")
 
 
