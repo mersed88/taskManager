7f2261cc0278ab3ee29c379bf81e08a7729e77f8
alex <sasha10041998@mail.ru>
2022-02-08 20:08:00 +0300

    add alembic

---
+++ alembic/versions/7fefbcb39903_first_commit.py
@@ -0,0 +1,24 @@
+"""first commit
+
+Revision ID: 7fefbcb39903
+Revises: 
+Create Date: 2022-02-08 20:03:37.571599
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '7fefbcb39903'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
